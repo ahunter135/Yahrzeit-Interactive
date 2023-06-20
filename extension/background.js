c3becb50a3fc87b7ac30d5ad36862235305c7a48
carterww <carterww@hotmail.com>
2023-06-20 10:11:33 -0500

    Might work?

--- extension/background.js
+++ extension/background.js
@@ -1,72 +1,58 @@
-/* Can maybe use some of this later, but I'm going to start from scratch right now
-
-
-const PWA_ORIGIN = 'https://www.yahrzeitinteractive.com/yahrzeit'//
-const PWA_PATH = '/yahrzeit.php?aid='
-
-const createUrl = assetId => {
-  const url = `${PWA_ORIGIN}${PWA_PATH}${assetId}`
-  chrome.storage.sync.set({ url, assetId })
-  return url
-}
-
-const destroyTabs = async url => {
-  const tabs = await chrome.tabs.query({ currentWindow: true })//.then(res => console.log({ res })) // TODO - close non-assetId tabs
-  const leaveOpen = tabs.find(tab => {
-    return url === tab.url
-  }) || tabs.find(({ url }) => {
-    const { origin } = new URL(url)
-    return origin === PWA_ORIGIN
-  })
-  console.log({ tabs })
-  chrome.tabs.remove(tabs.map(({ id }) => id).filter(id => id !== leaveOpen.id))
-  // tabs.forEach(tab => {
-  //   try {
-  //     if (tab.id !== leaveOpen.id) {
-  //       console.log('closing tab?')
-  //       chrome.tabs.discard(tab.id)
-  //     }
-  //   } catch (e) {
-  //     console.log('can not close tab')
-  //   }
-  // })
-}
-
-const redirect = async () => {
-  (chrome.enterprise || {
-    deviceAttributes: {
-      getDeviceAssetId: async callback => await callback()
-    }
-  }).deviceAttributes.getDeviceAssetId(async (assetId = 0) => {
-
-      // chrome.storage.local.set({}) //set to this if sync causes issues
-      const url = createUrl(assetId)
-      chrome.tabs.create({ url })
-      await destroyTabs(url)
-
-  })
-}
-
-chrome.runtime.onInstalled.addListener(redirect);
-
-chrome.runtime.onStartup.addListener(redirect)
-
-chrome.tabs.onCreated.addListener(async tab => {
-  const { url } = await chrome.storage.sync.get(['url'])
-  // chrome.tabs.create(({ url }))
-  await destroyTabs(url)
-  //await destroyTabs(url)
-})
+/**
+  Developer notes:
+    - In production, the manifest needs to have "kiosk_only": true, and the content_script's matches needs to match the PWA url only.
+      This ensures only the PWA can send messages to the background script.
 */
 
-// I don't think I need this
-/*const messageSrc = chrome.runtime.getURL("./scripts/messages.js");
 
-function importStuff() {
-  return importScripts(messageSrc);
-};
 
-let { restartDevice, startup } = importStuff();*/
+/**
+ * @description Restarts the device
+ * @param {*} request Request passed from the onMessageListener
+ * @param {*} sender Sender passed from the onMessageListener
+ * @returns Nothing
+ */
+const restartDevice = async function(request, sender) {
+  chrome.runtime.restart();
+  return undefined;
+}
+
+/**
+ * @description Startup function that returns the deviceAssetId and deviceSerialNumber to the message sender
+ * @param {*} request Request passed from the onMessageListener
+ * @param {*} sender Sender passed from the onMessageListener
+ * @returns An object with deviceData and possibly an errorMessage. deviceData contains the deviceAssetId and deviceSerialNumber.
+ * They are a string (double check this) or undefined
+ */
+const startup = async function(request, sender) {
+  // Can only run startUp if the extension has the enterprise.deviceAttributes permission
+  if (!(await chrome.permissions.contains({ permissions: ['enterprise.deviceAttributes'] }))) {
+    throw new Error("The extension does not have the enterprise.deviceAttributes permission");
+  }
+
+  const deviceData = {
+      deviceAssetId: undefined,
+      deviceSerialNumber: undefined
+  };
+
+  // NOTE: The chrome.enterprise.deviceAttributes only works if two conditions are met:
+  // 1. The device is running ChromeOS
+  // 2. The extension is pre-installed by policy
+  deviceData.deviceAssetId = await chrome.enterprise.deviceAttributes.getDeviceAssetId();
+  deviceData.deviceSerialNumber = await chrome.enterprise.deviceAttributes.getDeviceSerialNumber();
+
+  if (!deviceData.deviceAssetId || !deviceData.deviceSerialNumber) {
+      return { deviceData: undefined, errorMessage: 'deviceAssetId and/or deviceSerialNumber not found through chrome API' };
+  } else {
+      return { deviceData };
+  }
+}
+
+// Just using this to test the messaging system
+const log = async function(request, sender) {
+  console.log(request.message);
+  return request.message;
+}
 
 chrome.runtime.onMessage.addListener(
   /**
@@ -74,13 +60,14 @@ chrome.runtime.onMessage.addListener(
     @callback sendResponse Callback with a single parameter. A response object or undefined
   */
   async function(request, sender, sendResponse) {
-    console.log(request);
     let res = undefined;
 
     if (request.methodName === 'startup') {
       res = await startup(request, sender);
     } else if (request.methodName === 'restartDevice') {
       res = await restartDevice(request, sender);
+    } else if (request.methodName === 'log') {
+      res = await log(request, sender);
     }
     sendResponse(res);
   }
--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,5 +1,42 @@
-// This file can be used to test the extension. It should act like some script from the PWA.
+// This file mimics a PWA content script. It is used just to test the functionality of the messaging system for now.
+// After the PWA is created, a script similar to this would be used.
 // NOTE: In the manifest, need to set the matches for content scripts to the PWA url only
-setInterval(() => {
-    chrome.runtime.sendMessage({ methodName: 'log' }, response => { console.log(response) });
-  }, 2000);
+
+/**
+  A basic rundown of how the messaging system works:
+  1. The PWA sends a message to the background script with the following parameters:
+    - A request object that contains a methodName property and possibly other properties if necessary.
+    - A callback function that will be called with a response object or undefined as the arg
+  2. The extension receives this message and calls the corresponding function and calls the callback function
+  3. The PWA receives the response object or undefined through the callback function and can do whatever it wants with it
+*/
+
+/**
+ * @description Function that should be run on the PWA startup.
+ * Sends a message through the chrome runtime API and specifies a callback function with a response that 
+ * should contain the deviceAssetId and deviceSerialNumber
+ */
+const onKioskPWAStartup = async function() {
+  chrome.runtime.sendMessage({ methodName: 'startup' }, response => { console.log(response) });
+  setRestartDeviceTimeout();
+  
+  chrome.runtime.sendMessage({ methodName: "log", message: "Ran startup." }, response => { console.log(response) });
+}
+
+/**
+ * @description Function that sets a timeout to restart the device at 3am
+ * This should also be run by on the PWA's startup
+ */
+const setRestartDeviceTimeout = async function() {
+  var now = new Date();
+  var millisTill3 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 3, 0, 0, 0) - now;
+  if (millisTill3 < 0) {
+      millisTill3 += 86400000; // it's after 3am, try 3am tomorrow.
+  }
+  setTimeout(() => {
+    chrome.runtime.sendMessage({ methodName: 'restartDevice' }, response => { console.log(response) });
+  }, millisTill3);
+  console.log(`Device will restart in ${millisTill3} ms. Or ${millisTill3 / 1000 / 60 / 60} hours. Or ${millisTill3 / 1000 / 60 / 60 / 24} days.`)
+}
+
+onKioskPWAStartup();
