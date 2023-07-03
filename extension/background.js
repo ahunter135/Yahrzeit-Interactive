0423a5c2a20e42c423b0441c002e46eaeaacb351
carterww <carterww@hotmail.com>
2023-07-03 08:45:54 -0500

    Maybe 🤔

--- extension/background.js
+++ extension/background.js
@@ -1,78 +1,83 @@
 /**
-  Developer notes:
+    Developer notes:
     - In production, the manifest needs to have "kiosk_only": true, and the content_script's matches needs to match the PWA url only.
-      This ensures only the PWA can send messages to the background script.
-*/
+    This ensures only the PWA can send messages to the background script.
+    */
 
 
+
+    /**
+    * @description Restarts the device
+    * @param {*} request Request passed from the onMessageListener
+    * @param {*} sender Sender passed from the onMessageListener
+    * @returns Nothing
+    */
+    const restartDevice = async function(request, sender) {
+        chrome.runtime.restart();
+        return undefined;
+    }
 
 /**
- * @description Restarts the device
- * @param {*} request Request passed from the onMessageListener
- * @param {*} sender Sender passed from the onMessageListener
- * @returns Nothing
- */
-const restartDevice = async function(request, sender) {
-  chrome.runtime.restart();
-  return undefined;
-}
+    * @description Startup function that returns the deviceAssetId and deviceSerialNumber to the message sender
+    * @param {*} request Request passed from the onMessageListener
+    * @param {*} sender Sender passed from the onMessageListener
+    * @returns An object with deviceData and possibly an errorMessage. deviceData contains the deviceAssetId and deviceSerialNumber.
+    * They are a string (double check this) or undefined
+    */
+    const startup = async function(request, sender) {
+        // Can only run startUp if the extension has the enterprise.deviceAttributes permission
+        /*if (!(await chrome.permissions.contains({ permissions: ['enterprise.deviceAttributes'] }))) {
+            throw new Error("The extension does not have the enterprise.deviceAttributes permission");
+        }*/
 
-/**
- * @description Startup function that returns the deviceAssetId and deviceSerialNumber to the message sender
- * @param {*} request Request passed from the onMessageListener
- * @param {*} sender Sender passed from the onMessageListener
- * @returns An object with deviceData and possibly an errorMessage. deviceData contains the deviceAssetId and deviceSerialNumber.
- * They are a string (double check this) or undefined
- */
-const startup = async function(request, sender) {
-  // Can only run startUp if the extension has the enterprise.deviceAttributes permission
-  /*if (!(await chrome.permissions.contains({ permissions: ['enterprise.deviceAttributes'] }))) {
-    throw new Error("The extension does not have the enterprise.deviceAttributes permission");
-  }*/
+            const deviceData = {
+                deviceAssetId: undefined,
+                deviceSerialNumber: undefined
+            };
 
-  const deviceData = {
-    deviceAssetId: undefined,
-    deviceSerialNumber: undefined
-  };
+        // NOTE: The chrome.enterprise.deviceAttributes only works if two conditions are met:
+        // 1. The device is running ChromeOS
+        // 2. The extension is pre-installed by policy
+        try {
+            deviceData.deviceAssetId = await chrome.enterprise.deviceAttributes.getDeviceAssetId();
+            deviceData.deviceSerialNumber = await chrome.enterprise.deviceAttributes.getDeviceSerialNumber();
+        } catch (err) {
+            console.log(err);
+            return { deviceData: undefined, errorMessage: err.message };
+        }
 
-  // NOTE: The chrome.enterprise.deviceAttributes only works if two conditions are met:
-  // 1. The device is running ChromeOS
-  // 2. The extension is pre-installed by policy
-  try {
-    deviceData.deviceAssetId = await chrome.enterprise.deviceAttributes.getDeviceAssetId();
-    deviceData.deviceSerialNumber = await chrome.enterprise.deviceAttributes.getDeviceSerialNumber();
-  } catch (err) {
-    return { deviceData: undefined, errorMessage: err };
-  }
-
-  if (!deviceData.deviceAssetId || !deviceData.deviceSerialNumber) {
-      return { deviceData: undefined, errorMessage: 'deviceAssetId and/or deviceSerialNumber not found through chrome API' };
-  } else {
-      return { deviceData };
-  }
-}
+        if (!deviceData.deviceAssetId || !deviceData.deviceSerialNumber) {
+            return { deviceData: undefined, errorMessage: 'deviceAssetId and/or deviceSerialNumber not found through chrome API' };
+        } else {
+            return { deviceData };
+        }
+    }
 
 // Just using this to test the messaging system
 const log = async function(request, sender) {
-  console.log(request.message);
-  return request.message;
+    console.log(request.message);
+    return request.message;
 }
 
 chrome.runtime.onMessage.addListener(
-  /**
+    /**
     @summary Function responsible for listening to messages from the PWA.
     @callback sendResponse Callback with a single parameter. A response object or undefined
-  */
-  async function(request, sender, sendResponse) {
-    let res = undefined;
+    */
+    async function(request, sender, sendResponse) {
+        let res = undefined;
 
-    if (request.methodName === 'startup') {
-      res = await startup(request, sender);
-    } else if (request.methodName === 'restartDevice') {
-      res = await restartDevice(request, sender);
-    } else if (request.methodName === 'log') {
-      res = await log(request, sender);
+        if (request.methodName === 'startup') {
+            res = await startup(request, sender);
+        } else if (request.methodName === 'restartDevice') {
+            res = await restartDevice(request, sender);
+        } else if (request.methodName === 'log') {
+            res = await log(request, sender);
+        }
+        if (res == undefined) {
+            console.log("req didn't match any of these");
+        }
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        const response = chrome.tabs.sendMessage(tab.id, res);
     }
-    sendResponse(res);
-  }
 );
--- extension/scripts/content.js
+++ extension/scripts/content.js
@@ -1,60 +1,69 @@
 // This file mimics a PWA content script. It is used just to test the functionality of the messaging system for now.
-// After the PWA is created, a script similar to this would be used.
-// NOTE: In the manifest, need to set the matches for content scripts to the PWA url only
+    // After the PWA is created, a script similar to this would be used.
+    // NOTE: In the manifest, need to set the matches for content scripts to the PWA url only
 
 /**
-  A basic rundown of how the messaging system works:
-  1. The PWA sends a message to the background script with the following parameters:
+    A basic rundown of how the messaging system works:
+1. The PWA sends a message to the background script with the following parameters:
     - A request object that contains a methodName property and possibly other properties if necessary.
     - A callback function that will be called with a response object or undefined as the arg
-  2. The extension receives this message and calls the corresponding function and calls the callback function
-  3. The PWA receives the response object or undefined through the callback function and can do whatever it wants with it
-*/
+2. The extension receives this message and calls the corresponding function and calls the callback function
+3. The PWA receives the response object or undefined through the callback function and can do whatever it wants with it
+    */
+
+    /**
+    * @description Function that should be run on the PWA startup.
+    * Sends a message through the chrome runtime API and specifies a callback function with a response that 
+    * should contain the deviceAssetId and deviceSerialNumber
+    */
+    const onKioskPWAStartup = async function() {
+        alert('PWA has started up! Sending message to extension');
+
+        chrome.runtime.sendMessage({ methodName: 'startup' }, response => {  });
+        //setRestartDeviceTimeout();
+
+        //chrome.runtime.sendMessage({ methodName: "log", message: "Ran startup." }, response => { alert(response) });
+        alert('End of onKioskPWAStartup');
+    }
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request);
+        alert('In callback from extension. Response:' + request);
+        if (!request.deviceData) {
+            alert('deviceData is undefined.');
+            if (request.errorMessage) alert(request.errorMessage);
+            return;
+        }
+        if (!request.deviceData.deviceAssetId || !request.deviceData.deviceSerialNumber) {
+            alert('deviceAssetId and/or deviceSerialNumber not found through chrome API');
+            alert('Response.deviceData: ' + request.deviceData);
+            alert(`Response.deviceData.deviceAssetId: ${request.deviceData.deviceAssetId}`);
+            alert(`Response.deviceData.deviceSerialNumber: ${request.deviceData.deviceSerialNumber}`);
+            return;
+        }
+        if (request.errorMessage) {
+            alert(`Error: ${request.errorMessage}`);
+        }
+        alert('device asset id: ' + request.deviceData.deviceAssetId + '\ndevice serial number: ' + request.deviceData.deviceSerialNumber);
+
+    }
+);
 
 /**
- * @description Function that should be run on the PWA startup.
- * Sends a message through the chrome runtime API and specifies a callback function with a response that 
- * should contain the deviceAssetId and deviceSerialNumber
- */
-const onKioskPWAStartup = async function() {
-  alert('PWA has started up! Sending message to extension');
-
-  chrome.runtime.sendMessage({ methodName: 'startup' }, response => { 
-    alert('In callback from extension. Response:' + response);
-    if (!response.deviceData) {
-      alert('deviceData is undefined.');
-      if (response.errorMessage) alert(response.errorMessage);
-      return;
+    * @description Function that sets a timeout to restart the device at 3am
+    * This should also be run by on the PWA's startup
+    */
+    const setRestartDeviceTimeout = async function() {
+        var now = new Date();
+        var millisTill3 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 3, 0, 0, 0) - now;
+        if (millisTill3 < 0) {
+            millisTill3 += 86400000; // it's after 3am, try 3am tomorrow.
+        }
+        setTimeout(() => {
+            chrome.runtime.sendMessage({ methodName: 'restartDevice' }, response => { console.log(response) });
+        }, millisTill3);
+        console.log(`Device will restart in ${millisTill3} ms. Or ${millisTill3 / 1000 / 60 / 60} hours. Or ${millisTill3 / 1000 / 60 / 60 / 24} days.`)
     }
-    if (!response.deviceData.deviceAssetId || !response.deviceData.deviceSerialNumber) {
-      alert('deviceAssetId and/or deviceSerialNumber not found through chrome API');
-      alert('Response.deviceData: ' + response.deviceData);
-      alert(`Response.deviceData.deviceAssetId: ${response.deviceData.deviceAssetId}`);
-      alert(`Response.deviceData.deviceSerialNumber: ${response.deviceData.deviceSerialNumber}`);
-      return;
-    }
-    alert('device asset id: ' + response.deviceData.deviceAssetId + '\ndevice serial number: ' + response.deviceData.deviceSerialNumber);
-  });
-  //setRestartDeviceTimeout();
-  
-  //chrome.runtime.sendMessage({ methodName: "log", message: "Ran startup." }, response => { alert(response) });
-  alert('End of onKioskPWAStartup');
-}
-
-/**
- * @description Function that sets a timeout to restart the device at 3am
- * This should also be run by on the PWA's startup
- */
-const setRestartDeviceTimeout = async function() {
-  var now = new Date();
-  var millisTill3 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 3, 0, 0, 0) - now;
-  if (millisTill3 < 0) {
-      millisTill3 += 86400000; // it's after 3am, try 3am tomorrow.
-  }
-  setTimeout(() => {
-    chrome.runtime.sendMessage({ methodName: 'restartDevice' }, response => { console.log(response) });
-  }, millisTill3);
-  console.log(`Device will restart in ${millisTill3} ms. Or ${millisTill3 / 1000 / 60 / 60} hours. Or ${millisTill3 / 1000 / 60 / 60 / 24} days.`)
-}
 
 onKioskPWAStartup();
